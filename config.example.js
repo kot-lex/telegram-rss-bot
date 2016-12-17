1b1350a3883d87a10d8a33d3b0ea5c60ddece0ac
Aleksei Chernov <kot.lex@gmail.com>
2016-12-17 20:33:26 +0300

    Update sample config

--- config.example.js
+++ config.example.js
@@ -2,7 +2,13 @@ const config = {
   telegramOptions: {
     token: "YOURTOKENHERE"
   },
-  db: 'mongodb://localhost/bot'
+  db: 'mongodb://localhost/bot',
+  populateDB: {
+    cronRule: '*/5 * * * *' // Run every five minutes
+  },
+  sendMessages: {
+    cronRule: '*/10 * * * *' // Run every ten minutes
+  }
 };
 
 module.exports = config;
